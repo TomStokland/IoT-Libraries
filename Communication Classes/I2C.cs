2f03110084bb3bdfa5dfcb42a4cc3ca986ed7024
TomStokland <tomstokland@gmail.com>
2017-12-02 05:07:55 -0600

    Add files via upload

---
+++ Communication Classes/I2C.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+using Windows.Storage.Streams;
+
+namespace Communications
+{
+    public class I2C
+    {
+        public byte Address { get; set; } = 0x40;
+        public I2cBusSpeed Speed { get; set; } = I2cBusSpeed.StandardMode;
+        public I2cDevice device;
+
+        public I2C(byte address, I2cBusSpeed speed)
+        {
+            Initialize(address, speed);
+        }
+
+        public async void Initialize(byte address = 0x40, I2cBusSpeed speed = I2cBusSpeed.StandardMode)
+        {
+            //MainPage rootPage = MainPage.Current;
+
+            DeviceInformationCollection devices = null;
+            Address = address;
+            Speed = speed;
+
+            //await rootPage.uart.SendText("I2C Device at Address " + address.ToString() + " Initializing\n\r");
+
+            var settings = new I2cConnectionSettings(Address) { BusSpeed = Speed };
+            settings.SharingMode = I2cSharingMode.Shared;
+
+            // Get a selector string that will return all I2C controllers on the system 
+            string aqs = I2cDevice.GetDeviceSelector();
+
+            // Find the I2C bus controller device with our selector string
+            devices = await DeviceInformation.FindAllAsync(aqs);
+
+            //search for the controller
+            if (!devices.Any())
+                throw new IOException("No I2C controllers were found on the system");
+
+            //see if we can find the hat
+            device = await I2cDevice.FromIdAsync(devices[0].Id, settings);
+
+            //await rootPage.uart.SendText("I2C Device at Address " + address.ToString() + " Initialized\n\r");
+        }
+
+        public int Write(byte[] dataOut)
+        {
+            int success = 0;
+            try
+            {
+                device.Write(dataOut);
+            }
+            catch (Exception e)
+            {
+                success = e.HResult;
+            }
+
+            return success;
+        }
+
+        public byte[] WriteRead(byte[] dataOut, int numberOfBytesToRead)
+        {
+            byte[] dataIn = new byte[numberOfBytesToRead];
+            int success = 0;
+
+            try
+            {
+                device.WriteRead(dataOut, dataIn);
+            }
+            catch (Exception e)
+            {
+                success = e.HResult;
+                return null;
+            }
+
+            return dataIn;
+        }
+
+        public int WriteReadPartial(byte[] dataOut, int numberOfBytesToRead)
+        {
+            byte[] dataIn = new byte[numberOfBytesToRead];
+            int success = 0;
+            I2cTransferResult result = new I2cTransferResult();
+
+            try
+            {
+                result = device.WriteReadPartial(dataOut, dataIn);
+            }
+            catch (Exception e)
+            {
+                success = e.HResult;
+            }
+
+            return success;
+        }
+    }
+}
---
+++ Communication Classes/SPI.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Spi;
+using Windows.Devices.Enumeration;
+
+namespace Communications
+{
+    public class SPI
+    {
+        SpiDevice spiDevice;
+
+        public SPI(Int32 chipSelect = 0, Int32 speed = 10000000, SpiMode mode = SpiMode.Mode3, SpiSharingMode sharingMode = SpiSharingMode.Shared)
+        {
+            Initialize(chipSelect, speed, mode, sharingMode);
+        }
+
+        private async void Initialize(Int32 chipSelect, Int32 speed, SpiMode mode, SpiSharingMode sharingMode = SpiSharingMode.Shared)
+        {
+            DeviceInformation devInfo = null;
+            try
+            {
+                var settings = new SpiConnectionSettings(chipSelect);
+                settings.ClockFrequency = speed;
+                settings.Mode = mode;
+                settings.SharingMode = sharingMode;
+
+                try
+                {
+                    string aqs = SpiDevice.GetDeviceSelector();                     // Get a selector string that will return all SPI controllers on the system
+                    var dis = await DeviceInformation.FindAllAsync(aqs);            // Find the SPI bus controller devices with our selector string
+                    spiDevice = await SpiDevice.FromIdAsync(dis[0].Id, settings);   // Create an SpiDevice with our bus controller and SPI settings
+                    devInfo = dis[0];
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("SPI Initialization failed. Exception: " + e.Message + "\n\r");
+                }
+
+                if (spiDevice == null)
+                {
+                    throw new Exception("SPI Controller is currently in use by " +
+                        "another application. Please ensure that no other applications are using SPI.");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("SPI Initialization failed. Exception: " + ex.Message + "\n\r");
+            }
+
+        }
+
+        public void SetChipSelect(Int32 chipSelect) => spiDevice.ConnectionSettings.ChipSelectLine = chipSelect;
+        public void SetSpeed(Int32 speed) => spiDevice.ConnectionSettings.ClockFrequency = speed;
+        public void SetMode(SpiMode mode) => spiDevice.ConnectionSettings.Mode = mode;
+        public void SetSharingMode(SpiSharingMode sharingMode) => spiDevice.ConnectionSettings.SharingMode = sharingMode;
+
+        public byte[] Read(int numberOfBytesToRead)
+        {
+            byte[] readData = new byte[numberOfBytesToRead];
+
+            spiDevice.Read(readData);
+
+            return readData;
+        }
+
+        public byte[] TransferFullDuplex(byte[] writeData, int numberOfBytesToRead)
+        {
+            byte[] readData = new byte[numberOfBytesToRead];
+
+            spiDevice.TransferFullDuplex(writeData, readData);
+
+            return readData;
+        }
+
+        public byte[] Transfersequential(byte[] writeData, int numberOfBytesToRead)
+        {
+            byte[] readData = new byte[numberOfBytesToRead];
+
+            spiDevice.TransferSequential(writeData, readData);
+
+            return readData;
+        }
+
+        public void Write(byte[] writeData)
+        {
+            spiDevice.Write(writeData);
+        }
+
+    }
+}
